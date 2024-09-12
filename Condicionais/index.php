aa3a132514ff7b4cf0453fbad6daf23d1b77280e
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-09-12 20:39:06 -0300

    Exercicio 3 - Condicionais

--- Condicionais/index.php
+++ Condicionais/index.php
@@ -1,31 +1,16 @@
 <?php
 
-$count = 0;
+echo "Digite a idade da pessoa: ";
+$age = trim(fgets(STDIN));
 
-echo "Bem-vindo!\n";
-echo "Informe os votos do primeiro candidato: ";
-$candidato1 = trim(fgets(STDIN));
+$age = intval($age);
 
-echo "Informe os votos do segundo candidato: ";
-$candidato2 = trim(fgets(STDIN));
+if ($age < 16)
+echo "Nao eleitor ";
+else if ($age < 18 || $age > 65 )
+echo "Eleitor facultativo. ";
+else 
+echo "Eleitor obrigatorio";
 
-// Converte as entradas para inteiros
-$candidato1 = intval($candidato1);
-$candidato2 = intval($candidato2);
-
-// Compara os votos e exibe o resultado
-if ($candidato1 > $candidato2) {
-    echo "Candidato 1 ganhou!\n";
-} else if ($candidato1 < $candidato2) {
-    echo "Candidato 2 ganhou!\n";
-} else {
-    echo "Houve empate com $candidato1 votos\n";
-    ++$count;
-}
-
-if ($count == 0) {
-    echo "Votos do Candidato 1: $candidato1\n";
-    echo "Votos do Candidato 2: $candidato2\n";
-}
 
 ?>
