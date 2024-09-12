d4d8fea24066f0b96c9a0173e43dbc57e89c7a42
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-09-12 20:20:16 -0300

    Exercicio 2 - Condicionais

--- Condicionais/index.php
+++ Condicionais/index.php
@@ -1,21 +1,31 @@
 <?php
 
-echo "Informe o primeiro número: ";
-$number = trim(fgets(STDIN));
+$count = 0;
 
+echo "Bem-vindo!\n";
+echo "Informe os votos do primeiro candidato: ";
+$candidato1 = trim(fgets(STDIN));
 
-echo "Informe o segundo número: ";
-$number2 = trim(fgets(STDIN));
+echo "Informe os votos do segundo candidato: ";
+$candidato2 = trim(fgets(STDIN));
 
-// Converte as entradas para float
-$number = floatval($number);
-$number2 = floatval($number2);
+// Converte as entradas para inteiros
+$candidato1 = intval($candidato1);
+$candidato2 = intval($candidato2);
 
-if ($number > $number2) 
-    echo "O primeiro número ($number) é maior que o segundo número ($number2).\n";
- else if ($number < $number2) 
-    echo "O segundo número ($number2) é maior que o primeiro número ($number).\n";
- else 
-    echo "Ambos os números são iguais.\n";
+// Compara os votos e exibe o resultado
+if ($candidato1 > $candidato2) {
+    echo "Candidato 1 ganhou!\n";
+} else if ($candidato1 < $candidato2) {
+    echo "Candidato 2 ganhou!\n";
+} else {
+    echo "Houve empate com $candidato1 votos\n";
+    ++$count;
+}
+
+if ($count == 0) {
+    echo "Votos do Candidato 1: $candidato1\n";
+    echo "Votos do Candidato 2: $candidato2\n";
+}
 
 ?>
