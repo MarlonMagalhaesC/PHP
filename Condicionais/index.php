13e2785473f5069438c54cbdbf41e00186415e63
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-09-13 12:05:46 -0300

    Exercicio 4 - Condicionais

--- Condicionais/index.php
+++ Condicionais/index.php
@@ -1,16 +1,36 @@
 <?php
 
-echo "Digite a idade da pessoa: ";
-$age = trim(fgets(STDIN));
+echo "Bem vindo ao caixa eletronico!\nDigite o saldo atual da conta corrente: ";
+$amount = floatval(trim(fgets(STDIN)));
 
-$age = intval($age);
+if ($amount < 0)
+throw New Exception("Erro, o valor do saldo nao pode ser negativo. ");
 
-if ($age < 16)
-echo "Nao eleitor ";
-else if ($age < 18 || $age > 65 )
-echo "Eleitor facultativo. ";
-else 
-echo "Eleitor obrigatorio";
+echo "1)Saque\n2)Depósito\n3)Pagamento de Conta\n4)Transferência\nDigite uma operação:";
+$option = intval(trim(fgets(STDIN)));
 
+if ($option < 1 || $option > 4) 
+    throw New Exception("Opção inválida. Selecione uma opção entre 1 e 4.");   
+
+try {
+
+echo "Digite o valor da operação:";
+$value = floatval(trim(fgets(STDIN)));
+
+if ($value <= 0)
+throw New Exception("Somente valores positivos sao aceitos.");    
+
+if ($option == 2 )
+$amount+=$value;
+
+else if ($option != 2)
+$amount -= $value;
+
+}
+catch (Exception $e){
+  echo"Erro: " . $e->getMessage();
+}
+
+echo"Saldo final da conta $amount";
 
 ?>
