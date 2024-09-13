7ad18740fefd19c6f818a3d163ef4d8704fa6a9e
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-09-13 16:48:47 -0300

    Exercicio 2 - Repeticao

--- Repeticao/index.php
+++ Repeticao/index.php
@@ -4,7 +4,14 @@
 echo "Bem vindo!\nDigite n: ";
 $n = floatval(trim(fgets(STDIN)));
 
+echo "Os $n Primeiros pares: \n";
 for ($i = 1; $i<=$n; $i++)
 echo $i * 2 . " ";
+
+echo"\n";
+
+echo "Os $n Primeiros impares: \n";
+for ($i = 1; $i<=$n; $i++)
+echo $i * 2 - 1 . " ";
 
 ?>
