c0e073fe2e325d5b27528aa8fac567e3de36d0bb
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-09-26 15:30:08 -0300

    Tratando dados de formulario WEB 2

--- Xampp-WEB/geradorNumero.php
+++ Xampp-WEB/geradorNumero.php
@@ -4,20 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gerar automático </title>
+    <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <header>
-        <h1>Gerador de numeros</h1>
+        <h1>Resultado: </h1>
     </header>
+
     <main>
+
         <h1>Trabalhando com numeros aleatorios </h1>
         <p>Gerando um numero aleatorio entre 0 e 100.</p>
 
+        <?php
+        $numeroAleatorio = rand(1, 100); // Gera um número entre 1 e 100
+        echo "<p> <strong>Numero gerado: </strong> $numeroAleatorio </p>";
+
+        ?>
         <form action="geradorNumero.php" method="get">
-            <input type="submit" value="Gerar">
+            <input type="submit" value=" Gerar outro ">
         </form>
 
 
