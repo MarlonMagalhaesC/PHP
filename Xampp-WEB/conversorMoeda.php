9a7dad14bacd4b06063c703bf70cfcc2b5205c6d
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-10-01 15:57:13 -0300

    Melhorando conversor de moedas WEB

--- Xampp-WEB/conversorMoeda.php
+++ Xampp-WEB/conversorMoeda.php
@@ -4,24 +4,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moeda convertida</title>
+    <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
     <main>
-        <h1>Conversor de moeda R$ para $ </h1>
+        <h1>Conversor de moeda R$ para US$ </h1>
 
         <?php
 
-        $valorReal = (float)$_GET["inputReal"];
-        $valorCotacao = (float)$_GET["inputCotacao"];
+        $valorReal = $_GET["inputReal"];
+        $valorCotacao = $_GET["inputCotacao"];
+        $resDolar = $valorReal / $valorCotacao;
 
+        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
-        echo "<p> Valor <strong> R$: $valorReal </strong> </p>";
-        echo "<p> Valor cotacao <strong> US$: $valorCotacao </strong> </p>";
-        echo "<p> Valor convertido  para <strong> US$  " . $valorReal / $valorCotacao . "</strong> </p>";
+        echo "<p> Valor digitado <strong> " . numfmt_format_currency($padrao, $valorReal, "BRL") . " </strong> </p>";
+        echo "<p> Valor convertido  para <strong> " . numfmt_format_currency($padrao, $resDolar, "USD") . "</strong> </p>";
 
         ?>
         <button> <a href="javascript:history.go(-1)">Voltar à página anterior</a> </button>
