88e0dcc37431847e9146f5816a6e09657a17ee3f
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-10-01 21:37:27 -0300

    Usando API do Banco Central e JSON

--- Xampp-WEB/conversorMoeda.php
+++ Xampp-WEB/conversorMoeda.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Conversor R$ para US$</title>
+    <link rel="stylesheet" href="../ex006/style.css">
 </head>
 
 <body>
@@ -15,13 +15,17 @@
 
         <?php
 
+        require 'API.php'; // permite usar os codigos de outro arquivo php 
+
         $valorReal = $_GET["inputReal"];
-        $valorCotacao = $_GET["inputCotacao"];
+
+        $valorCotacao = $cotacao; // $cotacao ao inves de ser digitado pelo usario, agora é pego pela API do bacen.
+
         $resDolar = $valorReal / $valorCotacao;
 
         $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
-        echo "<p> Valor digitado <strong> " . numfmt_format_currency($padrao, $valorReal, "BRL") . " </strong> </p>";
+        echo " <p> Valor digitado <strong> " . numfmt_format_currency($padrao, $valorReal, "BRL") . " </strong> </p> <p>Cotação atual: <em> " .  numfmt_format_currency($padrao, $cotacao, "BRL") .  "</em></p>";
         echo "<p> Valor convertido  para <strong> " . numfmt_format_currency($padrao, $resDolar, "USD") . "</strong> </p>";
 
         ?>
