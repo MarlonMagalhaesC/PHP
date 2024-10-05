b4a401a7b8aad5b9863627191788102336e51cf6
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-10-05 11:51:24 -0300

    Formulario retroalimentado, calculando raizes

--- Xampp-WEB/index.php
+++ Xampp-WEB/index.php
@@ -1,54 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculador Salário Minimo</title>
+    <title>Raízes de um número</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <?php
-    //var_dump($_SERVER);
-    $salarioMin = $_POST["salarioMin"] ?? 1;
-    $salario = $_POST["amount"] ?? 0;
-
+    $numero = $_POST["_numero"] ?? 1;
     ?>
+
     <header>
-        <h1>Bem vindo!</h1>
+        <h1>Bem-vindo!</h1>
     </header>
     <main>
-        <h1>Informe seu salário</h1>
+        <h1>Raízes de um número:</h1>
 
         <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
-
-            <label for="idSalario">Seu Salário (R$):</label>
-            <input type="number" name="amount" id="idSalario" step="0.01" min="0" value="<?= $salario ?>">
-
-            <label for="idSalario">Salário minimo atual (R$):</label>
-            <input type="number" name="salarioMin" id="idSalario" step="0.01" min="1" value="<?= $salarioMin ?>">
-
-            <input type="submit" value="Calcular">
+            <label for="idNum">Informe um número:</label>
+            <input type="number" name="_numero" id="idNum" placeholder="Digite aqui" min="1" value="<?= $numero ?>">
+            <input type="submit" value="Ver Raízes">
         </form>
     </main>
 
     <section>
-        <h1>Resultado Final</h1>
+        <h1>Resultado final:</h1>
+
         <?php
-        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+        $raizQ = number_format(sqrt($numero), 3, ",", ".");
+        $raizC = number_format(pow($numero, 1 / 3), 3, ",", ".");
 
-        $quantity =  intdiv($salario, $salarioMin);
-
-        $resto = (($salario / $salarioMin) - $quantity) * $salarioMin; //quero o resto com os decimais
-        echo "<p>Seu salario <em>" . numfmt_format_currency($padrao, $salario, "BRL") . " </em> equivale a  <strong>  $quantity  salários mínimos </strong> ";
-
-        if ($salario > $salarioMin) {
-            echo "+ <em>" . numfmt_format_currency($padrao, $resto, "BRL") . " </em>";
-        }
-
-        echo "</p>";
-
+        echo "<p>Analisando o número <strong>$numero</strong> temos:</p>      
+        <ul>
+            <li>A sua raiz quadrada é <strong>$raizQ</strong></li>
+            <li>A sua raiz cúbica é <strong>$raizC</strong></li>
+        </ul>";
         ?>
     </section>
 </body>
