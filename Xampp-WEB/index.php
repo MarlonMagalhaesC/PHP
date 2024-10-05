fdc8633b91b0dd282f6421b5faf1ec1cc56592d6
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2024-10-05 13:41:01 -0300

    Formulario retroalimentado, calculando media com pesos

--- Xampp-WEB/index.php
+++ Xampp-WEB/index.php
@@ -4,41 +4,60 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Raízes de um número</title>
+    <title>Calculando Médias</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <?php
-    $numero = $_POST["_numero"] ?? 1;
+    $valor1 = $_REQUEST["_valor1"] ?? '';
+    $valor2 = $_REQUEST["_valor2"] ?? '';
+    $peso1 = $_REQUEST["_peso1"] ?? '';
+    $peso2 = $_REQUEST["_peso2"] ?? '';
+
     ?>
 
     <header>
-        <h1>Bem-vindo!</h1>
+        <h1>Bem vindo!</h1>
     </header>
+
     <main>
-        <h1>Raízes de um número:</h1>
+        <h1>Calculando Média Aritmética</h1>
 
         <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
-            <label for="idNum">Informe um número:</label>
-            <input type="number" name="_numero" id="idNum" placeholder="Digite aqui" min="1" value="<?= $numero ?>">
-            <input type="submit" value="Ver Raízes">
+            <label for="idvalor1">1° Valor:</label>
+            <input type="number" name="_valor1" id="idvalor1" min="1" placeholder="Digite aqui" value="<?= $valor1 ?>"
+                required>
+
+            <label for="idpeso1">Peso do 1° Valor:</label>
+            <input type="number" name="_peso1" id="idpeso1" min="1" placeholder="Digite aqui" value="<?= $peso1 ?>"
+                required>
+
+            <label for="idvalor2">2° Valor:</label>
+            <input type="number" name="_valor2" id="idvalor2" min="1" placeholder="Digite aqui" value="<?= $valor2 ?>"
+                required>
+
+            <label for="idPeso2">Peso do 2° Valor:</label>
+            <input type="number" name="_peso2" id="idPeso2" min="1" placeholder="Digite aqui" value="<?= $peso2 ?>"
+                required>
+            <input type="submit" value="Calcular">
         </form>
     </main>
 
     <section>
-        <h1>Resultado final:</h1>
+        <?php if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-        <?php
-        $raizQ = number_format(sqrt($numero), 3, ",", ".");
-        $raizC = number_format(pow($numero, 1 / 3), 3, ",", ".");
+            $mediaS = ($valor1 + $valor2) / 2;
+            $mediaP = (($valor1 * $peso1) + ($valor2 * $peso2)) / ($peso1 + $peso2);
 
-        echo "<p>Analisando o número <strong>$numero</strong> temos:</p>      
+            echo "<p>Analisando os valores <strong>$valor1</strong> e <strong>$valor2</strong>:</p>
         <ul>
-            <li>A sua raiz quadrada é <strong>$raizQ</strong></li>
-            <li>A sua raiz cúbica é <strong>$raizC</strong></li>
+            <li>A <strong>média aritmética simples</strong> entre os valores é de <em>" . number_format($mediaS, 2, ",", ".") . "</em></li>
+            <li>A <strong>média aritmética ponderada</strong> entre os valores é de <em>" . number_format($mediaP, 2, ",", ".") . "</em></li>
         </ul>";
+        }
         ?>
+
     </section>
 </body>
 
